e3511cf06bc0fea31695c64996fe4642677ce474
Nepochatova-Polina <54804662+Nepochatova-Polina@users.noreply.github.com>
2020-09-29 21:24:30 +0300

    Update utility.h

--- utility.h
+++ utility.h
@@ -14,7 +14,7 @@ struct Book {
 struct Reader {
     int cardNum;
     char name[30];
-   char surname[30];
+    char surname[30];
     int BookID;
 };
 
@@ -30,31 +30,38 @@ typedef struct Relation {
     struct Relation *next;
 } Relation;
 
-void writeIntoReaders(Relation *Head, Node *head, int max );
+struct Reader writeIntoReaders(Relation *Head, Node *head, int max);
+Node *writeIntoBook(Node *head, int max);
 
-void writeIntoBook( Node *head, int max);
 
-void readBooks();
+Node * delFromBook(Node *head);
+Node * delFromReader(Node *head, Relation *Head);
+void get_s(Node *ReaderID);
+struct Reader get_m( Node *ReaderID, int num);
 
-void readReaders();
 
 Node *readArr(int flag);
+Relation *readRelArr();
+Relation *pushIntoRel(Relation *head, int cardNum, int BookID);
+Node *pushBack(Node *head, int id, int pos);
 
-Node *pushBack(Node *head, Node s[]);
+void updateBook(Node *s);
+void updateReader(Node *s);
 
-void get_s(int num,Node *ReaderID);
+int findMaxID(Node *ID);
 
-struct Reader get_m(int num,Node *ReaderID);
-
-Relation *pushIntoRel(Relation *head, Relation s[] );
-
- Relation *readRelArr();
-
-int findMaxID( Node *ID);
-
-void del_m(Node *head, Relation *Head, int index, int flag);
 
 void writeIDIntoFile(Node *head, int flag);
-
 void writeIDIntoRealFile(Relation *head);
+void readBookByList(Node *head);
+void readReaderByList(Node *head);
+
+
+Relation *writeFileIntoRelList(const int arr[], int size);
+Node *writeFileIntoList(const int arr[], int size);
+Node *addelem(Node *lst, Node *s);
+Relation *addToRel(Relation *lst, Relation *s);
+void write(struct Node * head);
+void writeRel(struct Relation * head);
+
 #endif //LAB1_UTILITY_H
